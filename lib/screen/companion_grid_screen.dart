b8f60a711308c2aea5c4074e9a7e717a5d8b6f65
andrej286 <mikistojanovski6@gmial.com>
2023-03-22 22:43:45 +0100

    Grid setup * Added the grid system for the picture cards

--- lib/screen/companion_grid_screen.dart
+++ lib/screen/companion_grid_screen.dart
@@ -12,11 +12,36 @@ class CompanionGridScreen extends StatefulWidget {
 }
 
 class _CompanionGridScreen extends State<CompanionGridScreen> {
+
+  List<CompanionTile> myWidgets = [
+    CompanionTile(),
+    CompanionTile(),
+    CompanionTile(),
+    CompanionTile(),
+    CompanionTile(),
+    CompanionTile(),
+    CompanionTile(),
+    CompanionTile(),
+    CompanionTile()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Companions'), backgroundColor: customGreen),
-        backgroundColor: customGreen,
-        body: CompanionTile());
+        backgroundColor: Colors.white,
+        body: GridView.builder(
+          itemCount: myWidgets.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 8.0,
+            crossAxisSpacing: 8.0,
+            childAspectRatio: 0.68,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return myWidgets[index];
+          },
+          padding: const EdgeInsets.only(left: 10, right: 5),
+        ));
   }
 }
--- lib/widget/componion_tile.dart
+++ lib/widget/componion_tile.dart
@@ -4,65 +4,58 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class CompanionTile extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-            height: 270.0,
-            width: 150.0,
-            child: Card(
-                elevation: 4.0,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+    return Card(
+            elevation: 8.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 150.0,
+                  width: 150.0,
+                  padding: EdgeInsets.only(top: 10),
+                  child: Ink.image(
+                    image: NetworkImage(
+                        'https://images.hindustantimes.com/img/2022/11/20/1600x900/Fh8-GrTWQAMEno8_1668910522750_1668910539398_1668910539398.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text('Lora'),
+                ),
+                ButtonBar(
                   children: [
-                    Container(
-                      height: 150.0,
-                      width: 150.0,
-                      child: Ink.image(
-                        image: NetworkImage(
-                            'https://images.hindustantimes.com/img/2022/11/20/1600x900/Fh8-GrTWQAMEno8_1668910522750_1668910539398_1668910539398.jpg'),
-                        fit: BoxFit.cover,
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/home');
+                      },
+                      child: Text(
+                        'Details',
+                        style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900),
                       ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(16.0),
-                      alignment: Alignment.centerLeft,
-                      child: Text('Lora'),
-                    ),
-                    ButtonBar(
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/home');
-                          },
-                          child: Text(
-                            'Details',
-                            style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w900),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(customGreen),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStatePropertyAll<Color>(customGreen),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
-                            padding: MaterialStateProperty.all<EdgeInsets>(
-                              EdgeInsets.symmetric(vertical: 5, horizontal: 7),
-                            ),
-                          ),
-                        )
-                      ],
+                        ),
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.symmetric(vertical: 5, horizontal: 7),
+                        ),
+                      ),
                     )
                   ],
                 )
-            )
-        )
-    );
+              ],
+            ));
   }
 }
